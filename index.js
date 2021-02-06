5ef788ea0c8525a35dc730ca40bce94dd4ae9033
cyasmani <yasmanicastaneda@yahoo.com>
2021-02-05 23:05:41 -0800

    Added font styling

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var i = 0;
-var txt = 'Hi There. My name is Yasmani Castaneda and I am a Software Developer'; /* The text */
+var txt = 'Hi There! My name is Yasmani Castaneda and I am a Software Developer.'; /* The text */
 var speed = 60; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
   if (i < txt.length) {
+    document.getElementById("demo").style.fontFamily = "Gugi, cursive";
     document.getElementById("demo").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
